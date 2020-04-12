f2852715d47265f583bf677a1a41a12926306f10
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2020-04-12 15:33:18 -0400

    Initial Commit

---
+++ tomatoTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tomatoTimer
+//
+//  Created by jonah wilmsmeyer on 2020-04-12.
+//  Copyright © 2020 jonah wilmsmeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
